298f5f907b5d8918de41aa8d69dc08162d085a02
Razee <razee.hj@gmail.com>
2023-09-15 10:45:02 +0200

    Update MOPViewController

--- Framework/Framework/MOPViewController.swift
+++ Framework/Framework/MOPViewController.swift
@@ -8,6 +8,17 @@
 import UIKit
 
 public class MOPViewController: UIViewController {
+    let navigation: NavigateProtocol?
+    
+    init(navigation: NavigateProtocol?) {
+        self.navigation = navigation
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     public override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -15,5 +26,6 @@ public class MOPViewController: UIViewController {
 
     @IBAction func didPress(_ sender: Any) {
         print("did press")
+        self.navigation?.presentLogin()
     }
 }
