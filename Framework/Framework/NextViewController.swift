bd09f85e09e9fa2ea1950d17323bef1e7d02eea5
Pontus Croneld <pontus.croneld@umain.com>
2023-09-15 15:54:00 +0200

    Undo latest commit, issue is in parent view

--- Framework/Framework/NextViewController.swift
+++ Framework/Framework/NextViewController.swift
@@ -58,7 +58,7 @@ public class NextViewController: UIViewController {
     }
     
     @objc func pressedButton() {
-        navigationController?.popToRootViewController(animated: false)
+        self.dismiss(animated: false)
         navigationProtocol.presentSomeOtherView()
     }
 }
